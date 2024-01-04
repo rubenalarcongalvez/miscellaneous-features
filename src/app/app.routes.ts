d5413f73ac97320e4173a404f54620f23ec2e9c7
Rubén Alarcón Gálvez <rubenalarcongalvez@gmail.com>
2024-01-04 02:17:23 +0100

    all done until normalize strings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { NormalizeStringsComponent } from './views/normalize-strings/normalize-strings.component';
+import { PaymentWallComponent } from './views/payment-wall/payment-wall.component';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', component: HomeComponent},
     {path: 'normalize-strings', component: NormalizeStringsComponent},
+    {path: 'payment-wall', component: PaymentWallComponent},
 ];
---
+++ src/app/views/payment-wall/payment-wall.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-wall',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-wall.component.html',
+  styleUrl: './payment-wall.component.scss'
+})
+export class PaymentWallComponent {
+
+}
