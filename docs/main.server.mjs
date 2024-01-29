0d70a4797a9c36e73613c3d02d0d386286174b21
Rubén Alarcón Gálvez <rubenalarcongalvez@gmail.com>
2024-01-29 21:40:16 +0100

    glass effect fix deploy

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-EZ3LITEA.mjs";import"./chunk-NRYWDB7W.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-7ZG54V3H.mjs";import"./chunk-NRYWDB7W.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
