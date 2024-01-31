58ed11e505aedbec06abf7dc716fbf15f14991d7
Rubén Alarcón Gálvez <rubenalarcongalvez@gmail.com>
2024-01-31 21:18:37 +0100

    resize feature

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VXXSI4IF.mjs";import"./chunk-NRYWDB7W.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-GRKNMPF2.mjs";import"./chunk-NRYWDB7W.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
