adb7ae37327d4001fd454e85acf5beba46b78e14
Rubén Alarcón Gálvez <rubenalarcongalvez@gmail.com>
2024-03-28 18:42:38 +0100

    video and image in text

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-A5QST25W.mjs";import"./chunk-XYSBLA2Y.mjs";import"./chunk-OZ26XUUQ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-SA66TBOP.mjs";import"./chunk-XYSBLA2Y.mjs";import"./chunk-OZ26XUUQ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/views/text-with-video-or-image-background/text-with-video-or-image-background.component.ts
+++ src/app/views/text-with-video-or-image-background/text-with-video-or-image-background.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-text-with-video-or-image-background',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './text-with-video-or-image-background.component.html',
   styleUrl: './text-with-video-or-image-background.component.scss'
 })
 export class TextWithVideoOrImageBackgroundComponent {
-
+  
 }
