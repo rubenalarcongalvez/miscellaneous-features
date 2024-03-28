728dbf1a36d7a86f5cc8e587e1ed50edeb0ade83
Rubén Alarcón Gálvez <rubenalarcongalvez@gmail.com>
2024-03-28 14:02:31 +0100

    quick fix

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XAYSQLSE.mjs";import"./chunk-3IZOC2FV.mjs";import"./chunk-Q63QNLPS.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-4JY7GA2K.mjs";import"./chunk-3IZOC2FV.mjs";import"./chunk-Q63QNLPS.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
