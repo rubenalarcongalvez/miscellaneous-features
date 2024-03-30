ed4b5613ed78bec35ba844ef6492a3652062ea34
Rubén Alarcón Gálvez <rubenalarcongalvez@gmail.com>
2024-03-30 13:03:54 +0100

    dinamically change between light and dark mode

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4FTN2CDU.mjs";import"./chunk-XYSBLA2Y.mjs";import"./chunk-OZ26XUUQ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-UQRVXOJX.mjs";import"./chunk-XYSBLA2Y.mjs";import"./chunk-OZ26XUUQ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { MaterialModule } from '../../page/style/material/material.module';
 import { CommonModule } from '@angular/common';
+import { GlobalService } from '../../page/services/global.service';
 
 @Component({
   selector: 'app-header',
@@ -12,10 +13,16 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   sidenavFullscreen: boolean = false;
+  checkedDarkMode: boolean = false;
 
   constructor(private router: Router) {}
 
   get activeTab(): string {
     return this.router.url.replaceAll('-', ' ').replace('/', '');
+  }
+
+  toggleDarkScheme() {
+    this.checkedDarkMode = !this.checkedDarkMode;
+    this.checkedDarkMode ? document.documentElement.style.colorScheme = 'dark' : document.documentElement.style.colorScheme = 'light';
   }
 }
