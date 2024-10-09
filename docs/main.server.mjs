5e50021b3b1a9eaf29af983e61eccc385a31c77f
rubenalarcongalvez <rubenalarcongalvez@gmail.com>
2024-10-09 12:18:42 +0200

    hotfix boton google

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{b as a}from"./chunk-FYW4FHBE.mjs";import"./chunk-GQ3P7XKF.mjs";import"./chunk-Q6CQTCOX.mjs";import"./chunk-W4V7S2UK.mjs";export{a as default};
+import{b as a}from"./chunk-UKILSCEK.mjs";import"./chunk-GQ3P7XKF.mjs";import"./chunk-Q6CQTCOX.mjs";import"./chunk-W4V7S2UK.mjs";export{a as default};
